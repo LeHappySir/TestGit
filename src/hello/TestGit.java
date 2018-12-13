1bb8f9daa25150e133222a1dbb6df7aa1163a3df
imooc <xianhongle95@163.com>
2018-12-13 14:23:43 +0800

    add method

--- src/hello/TestGit.java
+++ src/hello/TestGit.java
@@ -1,9 +1,17 @@
 package hello;
 
 /**
- * 2.0
+ * 3.0
  */
 public class TestGit {
     private int age;
     private String name;
+
+    TestGit(int age,String name){
+        this.age = age;
+        this.name = name;
+    }
+    private void testMethod(){
+
+    }
 }
